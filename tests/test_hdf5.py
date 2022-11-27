2e517a21115618f8a8e1fdcd3dfe95a89097d2dc
Kacper Derlatka <kacper.derlatka@pega.com>
2022-11-28 00:41:58 +0100

    use numba_mpi bcast()

--- tests/test_hdf5.py
+++ tests/test_hdf5.py
@@ -3,20 +3,28 @@
 from pathlib import Path
 
 import h5py
-import numba_mpi
+import numba_mpi as mpi
 from mpi4py import MPI
+import numpy as np
 
 
 def test_hdf5(tmp_path):
-    rank = MPI.COMM_WORLD.rank  # The process ID (integer 0-3 for 4-process run)
-    tmp_path = MPI.COMM_WORLD.bcast(tmp_path, root=0)
+    rank = mpi.rank()  # The process ID (integer 0-3 for 4-process run)
 
-    path = Path(tmp_path) / "parallel_test.hdf5"
+    tmp_path = np.array(str(tmp_path), "c")
+    tmp_path2 = np.empty_like(tmp_path)
+    if rank == 0:
+        tmp_path2 = tmp_path
 
-    file = h5py.File(path, "w", driver="mpio", comm=MPI.COMM_WORLD)
+    mpi.bcast(tmp_path2, 0)
+    path = Path(tmp_path2.tobytes().decode("utf-8")) / "parallel_test.hdf5"
 
-    dset = file.create_dataset("test", (numba_mpi.size(),), dtype="i")
+    with h5py.File(path, "w", driver="mpio", comm=MPI.COMM_WORLD) as file:
+        dset = file.create_dataset("test", (mpi.size(),), dtype="i")
+        dset[rank] = rank
+        mpi.barrier()
+        file.close()
 
-    dset[rank] = rank
+    with h5py.File(path, 'r') as file:
+        assert list(file["test"]) == list(range(0, mpi.size()))
 
-    file.close()
