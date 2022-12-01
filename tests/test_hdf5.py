45f1fb7f3b581eec4d2f3d39f940b926841f5c32
Kacper Derlatka <kacper.derlatka@pega.com>
2022-12-01 11:02:52 +0100

    add jitted barrier test

--- tests/test_hdf5.py
+++ tests/test_hdf5.py
@@ -3,27 +3,39 @@
 from pathlib import Path
 
 import h5py
+import numba
 import numba_mpi as mpi
-from mpi4py import MPI
 import numpy as np
+import pytest
+from mpi4py import MPI
 
 
-def test_hdf5(tmp_path):
+@numba.njit()
+def jit_step(dset):
+    rank = mpi.rank()
+    dset[rank] = rank
+    mpi.barrier()
+
+
+@pytest.mark.parametrize("step", (jit_step, jit_step.py_func))
+def test_hdf5(tmp_path, sbtep):
     rank = mpi.rank()  # The process ID (integer 0-3 for 4-process run)
 
-    tmp_path = np.array(str(tmp_path), "c")
-    tmp_path2 = np.empty_like(tmp_path)
-    if rank == 0:
-        tmp_path2 = tmp_path
+    path_data = np.array(str(tmp_path), "c")
+    path = np.empty_like(path_data) if rank != 0 else path_data
 
-    mpi.bcast(tmp_path2, 0)
-    path = Path(tmp_path2.tobytes().decode("utf-8")) / "parallel_test.hdf5"
+    mpi.bcast(path, 0)
+
+    path = Path(path.tobytes().decode("utf-8")) / "parallel_test.hdf5"
 
     with h5py.File(path, "w", driver="mpio", comm=MPI.COMM_WORLD) as file:
         dset = file.create_dataset("test", (mpi.size(),), dtype="i")
-        dset[rank] = rank
-        mpi.barrier()
+
+        tmp = dset[:]
+        step(tmp)
+        dset[:] = tmp
+
         file.close()
 
-    with h5py.File(path, 'r') as file:
+    with h5py.File(path, "r") as file:
         assert list(file["test"]) == list(range(0, mpi.size()))
