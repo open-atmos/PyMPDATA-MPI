de82d567d1e9f0bfbceb60eae2f41f91956815ac
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2022-12-01 11:35:53 +0100

    simplify code

--- tests/test_hdf5.py
+++ tests/test_hdf5.py
@@ -17,11 +17,8 @@ def step(dset):
 
 
 def test_hdf5(tmp_path):
-    rank = mpi.rank()  # The process ID (integer 0-3 for 4-process run)
-
     path_data = np.array(str(tmp_path), "c")
-    path = np.empty_like(path_data) if rank != 0 else path_data
-
+    path = np.empty_like(path_data) if mpi.rank() != 0 else path_data
     mpi.bcast(path, 0)
 
     path = Path(path.tobytes().decode("utf-8")) / "parallel_test.hdf5"
@@ -32,8 +29,6 @@ def test_hdf5(tmp_path):
         tmp = dset[:]
         step(tmp)
         dset[:] = tmp
-
-        file.close()
 
     with h5py.File(path, "r") as file:
         assert list(file["test"]) == list(range(0, mpi.size()))
