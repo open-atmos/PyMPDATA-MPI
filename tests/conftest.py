6bd3c0fc1b64b8313151643243a229cba59c5901
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-03-27 20:50:28 +0200

    drop Intel MPI for now (+cleanup)

--- tests/conftest.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=missing-module-docstring,missing-function-docstring
-
-from mpi4py import MPI
-
-
-# pylint: disable=unused-argument
-def pytest_sessionstart(session):
-    if not MPI.Is_initialized():
-        MPI.Init()
