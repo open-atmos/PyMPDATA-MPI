9156d795a897fb59c0c59f77c84314487b9b14fa
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-03-27 12:47:49 +0200

    revert explicit init code

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -1,6 +1,5 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring
 
-import mpi4py
 import numba_mpi as mpi
 import numpy as np
 import pytest
@@ -8,8 +7,6 @@ import pytest
 
 @pytest.fixture
 def mpi_tmp_path(tmp_path):
-    if not mpi.initialized():
-        mpi4py.MPI.Init()
     path_data = np.array(str(tmp_path), "c")
     path = np.empty_like(path_data) if mpi.rank() != 0 else path_data
     mpi.bcast(path, 0)
