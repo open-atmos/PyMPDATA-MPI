d8ef5a9f85dc10aaa1fe34b8e1b5ca39ad4d72df
Sylwester Arabas <sylwester.arabas@uj.edu.pl>
2023-04-25 14:14:32 +0200

    uncomment all test cases

--- tests/test_2d.py
+++ tests/test_2d.py
@@ -67,16 +67,16 @@ class ReadmeSettings(Settings):
 
 OPTIONS_KWARGS = (
     {"n_iters": 1},
-    # {"n_iters": 2},
-    # {"n_iters": 2, "third_order_terms": True},
-    # {"n_iters": 2, "nonoscillatory": True},
+    {"n_iters": 2},
+    {"n_iters": 2, "third_order_terms": True},
+    {"n_iters": 2, "nonoscillatory": True},
     {"n_iters": 3},
 )
 
 COURANT_FIELDS = (
     (0.5, 0.25),
-    # (-0.5, 0.25),
-    # (0.5, -0.25),
+    (-0.5, 0.25),
+    (0.5, -0.25),
     (-0.5, -0.25),
 )
 
