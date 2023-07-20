2ecf9d0f465f34a35bef4b57d2c3509328893dd9
Kacper Derlatka <plgderlk@login01.ares.cyfronet.pl>
2023-07-20 12:22:12 +0200

    fix pymodelrunner example

--- examples/pymodelrunner.py
+++ examples/pymodelrunner.py
@@ -1,41 +1,50 @@
 """ example of integration with the py-modelrunner package """
+import mpi4py
 from pathlib import Path
+from typing import Optional, Dict, Any
 
 import numba_mpi
 from modelrunner import ModelBase, submit_job
 from PyMPDATA import Options
 
+from PyMPDATA_MPI.hdf_storage import HDFStorage
 import scenarios
 from PyMPDATA_MPI.domain_decomposition import subdomain
-from PyMPDATA_MPI.hdf_storage import HDFStorage
+from PyMPDATA_MPI.utils import setup_dataset_and_sync_all_workers
 
 
 class SimulationModel(ModelBase):
-    def __init__(self):
-        super().__init__()
-        scenario_class = getattr(scenarios, self.parameters["scenario"])
+    def __init__(self,
+        parameters: Optional[Dict[str, Any]] = None,
+        output: Optional[str] = None,
+        *,
+        strict: bool = False,
+    ):
+        super().__init__(parameters, output, strict=strict)
+        scenario_class = getattr(scenarios, parameters["scenario"])
         self.simulation = scenario_class(
-            mpdata_options=Options(**self.parameters["mpdata_options"]),
-            n_threads=self.parameters["n_threads"],
-            grid=self.parameters["grid"],
+            mpdata_options=Options(**parameters["mpdata_options"]),
+            n_threads=parameters["n_threads"],
+            grid=parameters["grid"],
             size=numba_mpi.size(),
-            courant_field_multiplier=self.parameters["courant_field"],
+            courant_field_multiplier=parameters["courant_field"],
             rank=numba_mpi.rank(),
         )
-        self.storage = HDFStorage.create_dataset(
-            name="psi",
-            path=Path(self.output),
-            grid=self.parameters["grid"],
-            steps=self.parameters["output_steps"],
+        HDFStorage.create_dataset(
+            name=parameters["dataset_name"],
+            path=Path(parameters["output_datafile"]),
+            grid=parameters["grid"],
+            steps=parameters["output_steps"],
         )
+        self.storage = HDFStorage.mpi_context(parameters["output_datafile"], "r+", mpi4py.MPI.COMM_WORLD)
 
     def __call__(self):
         steps = self.parameters["output_steps"]
         x_range = slice(
             *subdomain(self.parameters["grid"][0], numba_mpi.rank(), numba_mpi.size())
         )
-
-        self.simulation.advance(dataset=None, output_steps=steps, x_range=x_range)
+        dataset=setup_dataset_and_sync_all_workers(self.storage, self.parameters["dataset_name"])
+        return self.simulation.advance(dataset=dataset, output_steps=steps, x_range=x_range)
 
 
 if __name__ == "__main__":
@@ -47,8 +56,10 @@ if __name__ == "__main__":
             "n_threads": 1,
             "grid": (24, 24),
             "courant_field": (0.5, 0.5),
-            "output_steps": range(0, 25, 2),
+            "output_steps": tuple(i for i in range(0, 25, 2)),
+            "output_datafile": "output_psi.hdf5",
+            "dataset_name": "psi"
         },
-        output="data.hdf5",
+        output="output_times.hdf5",
         method="foreground",
     )
