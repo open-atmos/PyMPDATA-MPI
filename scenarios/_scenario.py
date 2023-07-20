e0da49d75ac7cdb53e376d222e740c9c28330c18
derlk <kacper.derlatka@pega.com>
2023-07-20 12:37:39 +0200

    return wall_time

--- scenarios/_scenario.py
+++ scenarios/_scenario.py
@@ -3,8 +3,11 @@ class _Scenario:
 
     def advance(self, dataset, output_steps, x_range):
         steps_done = 0
+        wall_time = 0
         for i1, output_step in enumerate(output_steps):
             n_steps = output_step - steps_done
-            self.solver.advance(n_steps=n_steps)
+            wall_time_per_timestep = self.solver.advance(n_steps=n_steps)
+            wall_time += wall_time_per_timestep * n_steps
             steps_done += n_steps
             dataset[x_range, :, i1] = self.solver.advectee.get()
+        return wall_time
